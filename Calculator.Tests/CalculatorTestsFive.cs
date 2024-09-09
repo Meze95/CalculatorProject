5e2cd66e9ab12248e43ecf63f335674e06a8de77
Meze95 <martinmichael1995@gmail.com>
2024-09-09 18:06:53 +0100

    Requirements:Step5

---
+++ Calculator.Tests/CalculatorTestsFive.cs
@@ -0,0 +1,38 @@
+﻿using CalculatorChallenge;
+
+namespace Calculator.Tests
+{
+    public class CalculatorTestsFive
+    {
+        private readonly Calculations _calculator;
+
+        public CalculatorTestsFive()
+        {
+            _calculator = new Calculations();
+        }
+
+        [Fact]
+        public void Add_ShouldIgnoreNumbersGreaterThan1000()
+        {
+            string input = "2,1001,6";
+            int result = _calculator.AddRequirementsFive(input);
+            Assert.Equal(8, result);
+        }
+
+        [Fact]
+        public void Add_ShouldReturnSumIgnoringMultipleLargeNumbers()
+        {
+            string input = "5,995,1500";
+            int result = _calculator.AddRequirementsFive(input);
+            Assert.Equal(1000, result);
+        }
+
+        [Fact]
+        public void Add_ShouldInclude1000InSum()
+        {
+            string input = "1001,1002,4,1000";
+            int result = _calculator.AddRequirementsFive(input);
+            Assert.Equal(1004, result);
+        }
+    }
+}
--- CalculatorChallenge/CalculatorChallenge/Program.cs
+++ CalculatorChallenge/CalculatorChallenge/Program.cs
@@ -10,7 +10,8 @@ class Program
             Console.WriteLine(calculator.AddRequirementsOne("1,5000"));
             Console.WriteLine(calculator.AddRequirementsTwo("1,2,3,4,5,6,7,8,9,10,11,12"));
             Console.WriteLine(calculator.AddRequirementsThree("1\n2,3,4,5,6\n7,8,9,10\n11,12"));
-            Console.WriteLine(calculator.AddRequirementsFour("1\n-4,5,-6"));
+            Console.WriteLine(calculator.AddRequirementsFour("1\n4,5,6"));
+            Console.WriteLine(calculator.AddRequirementsFive("2,1001,6"));
         }
         catch (ArgumentException ex)
         {
